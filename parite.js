d5f2604e197c9ce03ce6d852bdbaff15c3f728b5
Julien <julien.barras123@gmail.com>
2016-07-24 14:58:02 +0200

    Delete parite.js

--- parite.js
+++
@@ -1,7 +0,0 @@
-for (var i = 1; i <= 10; i++) { //tant que i est inf ou égal à 10 ; incrementation de la valeur de i
-    if (i % 2 === 0) {// Si ()
-        console.log(i + " est pair");
-    }else if (i % 2 !== 0){
-        console.log(i + " est impair");
-    }
-}
