7f72c9d0c42121ccce51f9ee5b1aa07465db5f30
Julien <julien.barras123@gmail.com>
2016-07-24 14:42:40 +0200

    Add files via upload

---
+++ presentation.js
@@ -0,0 +1,8 @@
+/* Un commentaire 
+sur plusieurs
+lignes */
+ 
+// Un commentaire sur une seule ligne
+
+console.log("Julien");
+console.log(18);
---
+++ valeurs.js
@@ -0,0 +1,9 @@
+/*
+Exercice : prévoir les valeurs affichées par ce programme
+*/
+
+console.log(4 + 5); //the result is 9 
+console.log("4 + 5");//the result is 4 +5
+console.log("4" + "5");//the result is 45
+console.log("4" +"\l" + "5");
+console.log("4" + "\n" + "5");
